daf23cd7e1f5630bed18efea83861aea8c685a00
Anatolii Richman <anatoly.rodin@webideal.pro>
2020-11-10 23:55:23 +0300

    Changed event details on close popup.

--- static/js/jspsych/plugins/jspsych-learning-vvr/jspsych-vvr-questions-right.js
+++ static/js/jspsych/plugins/jspsych-learning-vvr/jspsych-vvr-questions-right.js
@@ -335,7 +335,7 @@ jsPsych.plugins['survey-vvr-questions-right'] = (function() {
         response.trial_events.push({
           "event_type": 'popup closed',
           "event_raw_details": 'Close',
-          "event_converted_details": trial.event_converted_details,
+          "event_converted_details": trial.details.b.event_converted_details,
           "timestamp": jsPsych.totalTime(),
           "time_elapsed": jsPsych.totalTime() - timestamp_onload
         });
