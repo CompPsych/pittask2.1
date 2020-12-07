436ad7fd33d9bc1a953acd352b5563ab1d8df235
Serge- <enin.serge@gmail.com>
2020-12-07 21:57:33 +0200

    VOR basic functionality

--- static/js/jspsych/plugins/jspsych-vvr/jspsych-vor.js
+++ static/js/jspsych/plugins/jspsych-vvr/jspsych-vor.js
@@ -34,11 +34,25 @@ jsPsych.plugins["vor"] = (function () {
   };
 
   plugin.trial = function (display_element, trial) {
-    var OUTCOME = {
+
+    var extinct_duration_lockout = false;
+    var block_number = 0;
+    var interval_number = 0;
+    var OI_duration = OI_duration_A;
+    var OI_interval_timer;
+    var OI_threshold_interval;
+    var OI_interval = 0;
+
+    var outcome_collection = {
       MM: "/static/images/MM.png",
       TT: "/static/images/TT.png",
       BBQ: "/static/images/BBQ.png",
     };
+   
+    var outcome_arr =  jsPsych.randomization.shuffle([
+      outcome_collection[counter_balancing[0].left],
+      outcome_collection[counter_balancing[0].right]
+    ]);
 
     // store response
     var response = {
@@ -56,55 +70,89 @@ jsPsych.plugins["vor"] = (function () {
       '<div class="outcome-container"></div>' +
       "</div>";
 
+    // render
     display_element.innerHTML = new_html;
 
     var $outcome_container = $(".outcome-container");
 
+    function start_OI() {
+      clearTimeout(OI_interval_timer);
+
+      // display outcome
+      $outcome_container.html(
+        '<img class="outcome" src="' + outcome_arr[interval_number] + '"/>'
+      );
+
+      // clear outcome container
+      jsPsych.pluginAPI.setTimeout(function () {
+        $outcome_container.html("");
+      }, outcome_duration);
+
+      interval_number += 1;
+      if (interval_number === 2) {
+        interval_number = 0;
+        block_number += 1;
+        outcome_arr = jsPsych.randomization.shuffle([
+          outcome_collection[counter_balancing[0].left],
+          outcome_collection[counter_balancing[0].right],
+        ]);
+        threshold_interval();
+      }
+
+      OI_interval_timer = jsPsych.pluginAPI.setTimeout(function () {
+        // terminate VOR stage
+        if (block_number === VOR_block_num) {
+          clearTimeout(OI_interval_timer);
+          end_trial();
+        } else {
+          start_OI();
+        }
+      }, OI_duration + outcome_duration);
+    }
+
+
+    function reset_OI() {
+
+      clearTimeout(OI_interval_timer);
+
+      OI_interval_timer = jsPsych.pluginAPI.setTimeout(function () {
+        if (block_number === VOR_block_num) {
+          clearTimeout(OI_interval_timer);
+          end_trial();
+        } else {
+          start_OI();
+        }
+      }, OI_duration + outcome_duration);
+    }
+
+    function threshold_interval() {
+
+      clearTimeout(OI_threshold_interval);
+
+      OI_threshold_interval = jsPsych.pluginAPI.setTimeout(function() {
+        OI_duration = OI_duration_B;
+      }, OI_threshold * 1000);
+    }
+
+    // extinction interval
+    function extinct_interval() {
+    
+      clearTimeout(OI_interval);
+ 
+      if(!extinct_duration_lockout) {
+        OI_interval = jsPsych.pluginAPI.setTimeout(function() {
+          extinct_duration_lockout = true;
+          start_OI();
+          threshold_interval();
+        }, extinct_duration * 1000);
+      }
+    }
+    // call extinction interval
+    extinct_interval();
+   
+
     // function to handle responses by the subject
     var after_response = function (info) {
-      function machine_tilt() {
-        if (info.key === left_tilt) {
-          $(".vending-machine").css({
-            transform: "rotate(" + shake_left_rotate + "deg) translateX(" + shake_left_translateX + "%)",
-            transition: "all " + shake_transition + "s cubic-bezier(0.65, 0.05, 0.36, 1)",
-          });
-
-          jsPsych.pluginAPI.setTimeout(function () {
-            $(".vending-machine").css({
-              transform: "rotate(0deg) translateX(0%)",
-              transition: "all " + shake_transition + "s cubic-bezier(0.65, 0.05, 0.36, 1)",
-            });
-          }, shake_return_time);
-
-          response.trial_events.push({
-            event_type: "left tilt",
-            event_raw_details: shake_left_translateX + "%, " + shake_left_rotate + "deg",
-            event_converted_details: "vending machine was tilted left " + shake_left_translateX + "%, " + shake_left_rotate + "deg",
-            timestamp: jsPsych.totalTime(),
-            time_elapsed: jsPsych.totalTime() - timestamp_onload,
-          });
-        } else if (info.key === right_tilt) {
-          $(".vending-machine").css({
-            transform: "rotate(" + shake_right_rotate + "deg) translateX(" + shake_right_translateX + "%)",
-            transition: "all " + shake_transition + "s cubic-bezier(0.65, 0.05, 0.36, 1)",
-          });
-
-          jsPsych.pluginAPI.setTimeout(function () {
-            $(".vending-machine").css({
-              transform: "rotate(0deg) translateX(0%)",
-              transition: "all " + shake_transition + "s cubic-bezier(0.65, 0.05, 0.36, 1)",
-            });
-          }, shake_return_time);
-
-          response.trial_events.push({
-            event_type: "right tilt",
-            event_raw_details: shake_right_translateX + "%, " + shake_right_rotate + "deg",
-            event_converted_details: "vending machine was tilted right " + shake_right_translateX + "%, " + shake_right_rotate + "deg",
-            timestamp: jsPsych.totalTime(),
-            time_elapsed: jsPsych.totalTime() - timestamp_onload,
-          });
-        }
-      }
 
       if (info.key_release === undefined) {
         response.trial_events.push({
@@ -115,7 +163,7 @@ jsPsych.plugins["vor"] = (function () {
           time_elapsed: jsPsych.totalTime() - timestamp_onload,
         });
 
-        machine_tilt();
+        machine_tilt(info);
       } else {
         response.trial_events.push({
           event_type: "key release",
@@ -127,13 +175,66 @@ jsPsych.plugins["vor"] = (function () {
       }
     };
 
+    function machine_tilt(info) {
+
+      if (info.key === left_tilt) {
+        $(".vending-machine").css({
+          transform: "rotate(" + shake_left_rotate + "deg) translateX(" + shake_left_translateX + "%)",
+          transition: "all " + shake_transition + "s cubic-bezier(0.65, 0.05, 0.36, 1)",
+        });
+
+        jsPsych.pluginAPI.setTimeout(function () {
+          $(".vending-machine").css({
+            transform: "rotate(0deg) translateX(0%)",
+            transition: "all " + shake_transition + "s cubic-bezier(0.65, 0.05, 0.36, 1)",
+          });
+        }, shake_return_time);
+
+        response.trial_events.push({
+          event_type: "left tilt",
+          event_raw_details: shake_left_translateX + "%, " + shake_left_rotate + "deg",
+          event_converted_details: "vending machine was tilted left " + shake_left_translateX + "%, " + shake_left_rotate + "deg",
+          timestamp: jsPsych.totalTime(),
+          time_elapsed: jsPsych.totalTime() - timestamp_onload,
+        });
+
+      } else if (info.key === right_tilt) {
+        $(".vending-machine").css({
+          transform: "rotate(" + shake_right_rotate + "deg) translateX(" + shake_right_translateX + "%)",
+          transition: "all " + shake_transition + "s cubic-bezier(0.65, 0.05, 0.36, 1)",
+        });
+
+        jsPsych.pluginAPI.setTimeout(function () {
+          $(".vending-machine").css({
+            transform: "rotate(0deg) translateX(0%)",
+            transition: "all " + shake_transition + "s cubic-bezier(0.65, 0.05, 0.36, 1)",
+          });
+        }, shake_return_time);
+
+        response.trial_events.push({
+          event_type: "right tilt",
+          event_raw_details: shake_right_translateX + "%, " + shake_right_rotate + "deg",
+          event_converted_details: "vending machine was tilted right " + shake_right_translateX + "%, " + shake_right_rotate + "deg",
+          timestamp: jsPsych.totalTime(),
+          time_elapsed: jsPsych.totalTime() - timestamp_onload,
+        });
+      }
+
+      if (info.key === left_tilt || info.key === right_tilt) {
+        if(!extinct_duration_lockout) {
+           extinct_interval();
+        } else {
+          reset_OI();
+        }
+      }
+    }
+
     // function to end trial when it is time
     var end_trial = function () {
-      // clear popup timer
-      clearTimeout(popup_timer);
 
       // kill any remaining setTimeout handlers
       jsPsych.pluginAPI.clearAllTimeouts();
+      clearTimeout(OI_interval)
 
       // kill keyboard listeners
       if (typeof keyboardListener !== "undefined") {
