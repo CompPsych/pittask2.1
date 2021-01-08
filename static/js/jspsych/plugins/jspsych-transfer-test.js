a7009f19e941cb3225e503233ee559fdbc44986f
Amakina <merysmith05@gmail.com>
2021-01-08 09:55:16 +0400

    Add mouse tracking for transfer test

--- static/js/jspsych/plugins/jspsych-transfer-test.js
+++ static/js/jspsych/plugins/jspsych-transfer-test.js
@@ -92,7 +92,8 @@ jsPsych.plugins["transfer-test"] = (function() {
 
     // store responses, events
     var response = {
-      trial_events: []
+      trial_events: [],
+      mouse_events: [],
     };
 
     // number of repeats
@@ -244,11 +245,17 @@ jsPsych.plugins["transfer-test"] = (function() {
         jsPsych.pluginAPI.cancelKeyboardResponse(keyboardListener);
         jsPsych.pluginAPI.cancelClickResponse(clickListener);
       }
+      
+      // kill mouse listener
+      if (typeof mouseMoveListener !== 'undefined') {
+        jsPsych.pluginAPI.cancelMouseEnterResponse(mouseMoveListener);
+      }
 
       // gather the data to store for the trial
       var trial_data = {
           stage_name: JSON.stringify(trial.stage_name),
           events: JSON.stringify(response.trial_events),
+          mouse_events: JSON.stringify(response.mouse_events)
       };
 
       // clear the display
@@ -347,6 +354,19 @@ jsPsych.plugins["transfer-test"] = (function() {
       }
     };
 
+    // function to handle mouse hovering UI elements
+    var after_mousemove = function(info) {
+      console.log('target: ', info.target)
+      response.mouse_events.push({
+        x: info.x, 
+        y: info.y, 
+        viewport_size: info.viewport_size,
+        type: info.type,
+        target: info.target,
+        timestamp: jsPsych.totalTime(),
+      });
+    };
+
     // start the response listener
     if (trial.choices !== jsPsych.NO_KEYS) {
       var keyboardListener = jsPsych.pluginAPI.getKeyboardResponse({
@@ -365,6 +385,20 @@ jsPsych.plugins["transfer-test"] = (function() {
         allow_held_key: false
       });
     }
+
+    // identifiers for hover event targets
+    var elementsMapping = [
+      {
+          element: 'vending machine',
+          tag: ['rect', 'path'],
+      },
+    ];
+    
+    // start mouse move listener
+    var mouseMoveListener = jsPsych.pluginAPI.getMouseMoveResponse({
+      callback_function: after_mousemove,
+      elements_mapping: elementsMapping,
+    });
 
     // hide stimulus if stimulus_duration is set
     if (trial.stimulus_duration !== null) {
