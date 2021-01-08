b2a7c1507071b9c188b93449aa10f4b9f7664d42
Amakina <merysmith05@gmail.com>
2021-01-08 10:26:49 +0400

    Add mouse tracking for YIAT

--- static/js/jspsych/plugins/jspsych-survey-questionnaire/jspsych-YIAT.js
+++ static/js/jspsych/plugins/jspsych-survey-questionnaire/jspsych-YIAT.js
@@ -92,10 +92,14 @@ jsPsych.plugins['YIAT'] = (function () {
       
       var plugin_id_name = "jspsych-survey-multi-choice-YIAT";
       var html = "";
+      
+      // identifiers for hover event targets
+      var elementsMapping = [];
   
       // store responses, events
       var response = {
-        trial_events: []
+        trial_events: [],
+        mouse_events: [],
       };
       var timestamp_onload = jsPsych.totalTime();
   
@@ -178,6 +182,14 @@ jsPsych.plugins['YIAT'] = (function () {
             </ul>
         </div>`
   
+      var titles = ['Never', 'Rarely', 'Sometimes', 'Often', 'Very often']
+
+      for (var i = 0; i < titles.length; i++) {
+        elementsMapping.push({
+          element: 'A' + (i + 1),
+          text: [titles[i]]
+        });
+      }
   
       // generate question order. this is randomized here as opposed to randomizing the order of trial.questions
       // so that the data are always associated with the same question regardless of order
@@ -205,9 +217,9 @@ jsPsych.plugins['YIAT'] = (function () {
         html += '<div id="jspsych-survey-multi-choice-' + question_id + '" class="' + question_classes.join(' ') + '"  data-name="' + question.name + '">';
   
         // add question text
-        html += '<div class="jspsych-survey-multi-choice-option-left"><span class="jspsych-survey-multi-choice-number">' + (i + 1) + '.</span><p class="jspsych-survey-multi-choice-text survey-multi-choice jspsych-survey-multi-choice-question-text" style="text-align: left; padding: 0 10px; width: 100%;">' + question.prompt
+        html += '<div class="jspsych-survey-multi-choice-option-left"><span class="jspsych-survey-multi-choice-number">' + (i + 1) + '.</span><p class="jspsych-survey-multi-choice-text survey-multi-choice jspsych-survey-multi-choice-question-text" style="text-align: left; padding: 0 10px; width: 100%;"><span>' + question.prompt
         // question.required
-        html += '</p></div>';
+        html += '</span></p></div>';
         html += '<div class="jspsych-survey-multi-choice-option-right">';
   
         // create option radio buttons
@@ -224,9 +236,19 @@ jsPsych.plugins['YIAT'] = (function () {
           html += '<label hidden class="jspsych-survey-multi-choice-text" for="' + input_id + '">' + question.options[j] + '</label>';
           html += '<input hidden type="radio" name="' + input_name + '" data-time-stamp="Q' + (i+1) + '" data-question-number="Q' + (i+1) +'A' + (j+1) +'" id="' + input_id + '" class="form-radio" value="' + trial.title[j] + '" ' + required_attr + '></input>';
           html += '</div>';
+
+          elementsMapping.push({
+            element: 'Q' + (i + 1) + 'A' + (j + 1) + ' input',
+            id: [input_id]
+          });
         }
   
         html += '</div></div>';
+
+        elementsMapping.push({
+          element: 'Q' + (i + 1),
+          text: [(i + 1) + '.', question.prompt]
+        });
       }
   
       // add submit button
@@ -293,6 +315,19 @@ jsPsych.plugins['YIAT'] = (function () {
         }
       };
 
+      // function to handle mouse hovering UI elements
+      var after_mousemove = function(info) {
+        console.log('target: ', info.target)
+        response.mouse_events.push({
+          x: info.x, 
+          y: info.y, 
+          viewport_size: info.viewport_size,
+          type: info.type,
+          target: info.target,
+          timestamp: jsPsych.totalTime(),
+        });
+      }
+
        // save timestamp on input click
       $("input[type=radio]").on("click change touchstart",function(){
         var time_stamp_key = $(this).data('time-stamp'); 
@@ -344,6 +379,11 @@ jsPsych.plugins['YIAT'] = (function () {
             jsPsych.pluginAPI.cancelKeyboardResponse(keyboardListener);
             jsPsych.pluginAPI.cancelClickResponse(clickListener);
           }
+        
+          // kill mouse listener
+          if (typeof mouseMoveListener !== 'undefined') {
+            jsPsych.pluginAPI.cancelMouseEnterResponse(mouseMoveListener);
+          }
   
           // save data
           var trial_data = {
@@ -352,7 +392,8 @@ jsPsych.plugins['YIAT'] = (function () {
             "timestamp": JSON.stringify(timestamp_data),
             "time_stamp": JSON.stringify(trial.time_stamp),
             "question_order": JSON.stringify(question_order),
-            "events": JSON.stringify(response.trial_events)
+            "events": JSON.stringify(response.trial_events),
+            "mouse_events": JSON.stringify(response.mouse_events)
           };
   
           // clear the display
@@ -400,6 +441,24 @@ jsPsych.plugins['YIAT'] = (function () {
         rt_method: 'performance',
         persist: true,
         allow_held_key: false
+      });
+    
+      elementsMapping.push(
+        {
+          element: 'submit button',
+          value: [trial.button_label]
+        },
+        {
+          element: 'instruction text',
+          id: ['jspsych-survey-multi-choice-preamble']
+        }
+      );
+    
+      // start mouse move listener
+      var mouseMoveListener = jsPsych.pluginAPI.getMouseMoveResponse({
+        callback_function: after_mousemove,
+        elements_mapping: elementsMapping,
+        ignored_tags: ['p'],
       });
     };
   
