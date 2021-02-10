346d6fe11783dab62c3e70435cd80f66529727c6
Anatolii Richman <anatoly.rodin@webideal.pro>
2021-02-10 22:59:04 +0300

    Added fixes to the SS test.

--- static/js/jspsych/plugins/jspsych-survey-questionnaire/jspsych-SMOKING-STATUS.js
+++ static/js/jspsych/plugins/jspsych-survey-questionnaire/jspsych-SMOKING-STATUS.js
@@ -277,21 +277,6 @@ jsPsych.plugins['Smoking-Status'] = (function() {
       }
     };
 
-    // highlight input
-    $('.jspsych-survey-highlight').on('click', function() {
-      $(this).addClass('bg-primary');
-      $(this).next('input').prop('checked', true);
-    });
-
-    // forced click event fix for some laptops touchpad
-    $('label').on('click', function() {
-      var labelID = $(this).attr('for');
-
-      if ('labelID') {
-        $('#' + labelID).prop('checked', true).trigger('click').trigger('change');
-      }
-    });
-
     // save timestamp on input click
     $('input[type=radio]').on('click touchstart', function(event) {
       if (event.type === 'click' || event.type === 'touchstart') {
